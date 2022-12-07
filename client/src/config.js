82b6c30bb2f3924b50f79a076fecc19e7fdebe65
Trang <dothuytrang2312@gmail.com>
2022-12-07 11:19:04 +0100

    autopush

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,8 @@
-const URL = `http://localhost:3040`;
+// const URL = `http://localhost:3040`;
+
+const URL =
+    window.location.hostname === `localhost`
+        ? `http://localhost:3040` // 3040 should be replaced with your server port
+        : `http://159.89.11.31`; // it should be replaced with actual domain during the deployment
 
 export { URL };
